96b35880089ccd61ca846922706048a9e5501775
Margely Cornelissen <134381864+MaCuinea@users.noreply.github.com>
2025-01-20 11:48:02 +0100

    Update sequence_1_10_ch.py
    Remove not implemented warning

--- standalone_driving_system_software/interleaved_example/sequences/sequence_1_10_ch.py
+++ standalone_driving_system_software/interleaved_example/sequences/sequence_1_10_ch.py
@@ -80,7 +80,6 @@ def create_sequence_collection(logger):
     # based on the set focus.
     seq1.dephasing_degree = None  # [degrees]: None, [120] or [0, 135, 239, 90]
 
-    # THE FEATURE IS NOT ENABLED YET! Use amplitude only for now
     # either set maximum pressure in free water [MPa], voltage [V] or amplitude [%]
     seq1.press = 0.5  # [MPa], maximum pressure in free water
     # seq1.volt = 0  # [V], voltage per channel
